ffc262fcfb200f573824b0d426b3122c29b8e695
Steve Fillingham <steve.fillingham@gmail.com>
2017-04-20 11:17:37 +1000

    Changed new API to be Object Oriented (still maintains backwards compatibility)

--- www/VirtualManagerBLE.js
+++ www/VirtualManagerBLE.js
@@ -2,54 +2,54 @@ var exec = require('cordova/exec');
 
 var _module = "VirtualManagerBLE";
 
-function clientStartScanning(clientId, serviceUUIDs, options, success, error) {
-	exec(success, error, _module, "clientStartScanning", [clientId, serviceUUIDs, options]);
+function Client(clientId) {
+	this._clientId = clientId;
 }
 
-function clientStopScanning(clientId, success, error) {
-	exec(success, error, _module, "clientStopScanning", [clientId]);
+Client.prototype.startScanning = function (serviceUUIDs, options, success, error) {
+	exec(success, error, _module, "clientStartScanning", [this._clientId, serviceUUIDs, options]);
 }
 
-function clientSubscribeStateChange(clientId, success, error) {
-	exec(success, error, _module, "clientSubscribeStateChange", [clientId]);
+Client.prototype.stopScanning = function (success, error) {
+	exec(success, error, _module, "clientStopScanning", [this._clientId]);
 }
 
-function clientUnsubscribeStateChange(clientId, success, error) {
-	exec(success, error, _module, "clientUnsubscribeStateChange", [clientId]);
+Client.prototype.subscribeStateChange = function (success, error) {
+	exec(success, error, _module, "clientSubscribeStateChange", [this._clientId]);
 }
 
-function clientPeripheral_getServices(clientId, peripheral_id, success, error) {
-	exec(success, error, _module, "clientPeripheral_getServices", [clientId, peripheral_id]);
+Client.prototype.unsubscribeStateChange = function (success, error) {
+	exec(success, error, _module, "clientUnsubscribeStateChange", [this._clientId]);
+}
+
+Client.prototype.peripheral_getServices = function (peripheral_id, success, error) {
+	exec(success, error, _module, "clientPeripheral_getServices", [this._clientId, peripheral_id]);
 }
 
 // Updated API - clientXXX functions allow multiple JS clients to have their own BLE comms
-exports.clientStartScanning = clientStartScanning;
-exports.clientStopScanning = clientStopScanning;
-exports.clientSubscribeStateChange = clientSubscribeStateChange;
-exports.clientUnsubscribeStateChange = clientUnsubscribeStateChange;
-exports.clientPeripheral_getServices = clientPeripheral_getServices;
+exports.Client = Client;
 
 // Old API - only a single JS client can use this 
-var _defaultClientId = "Default";
+var _defaultClient = new Client("Default");
 
 exports.startScanning = function(serviceUUIDs, options, success, error) {
-	clientStartScanning(_defaultClientId, serviceUUIDs, options, success, error);
+	_defaultClient.startScanning(serviceUUIDs, options, success, error);
 }
 
 exports.stopScanning = function(success, error) {
-	clientStopScanning(_defaultClientId, success, error);
+	_defaultClient.stopScanning(success, error);
 }
 
 exports.subscribeStateChange = function(success, error) {
-	clientSubscribeStateChange(_defaultClientId, success, error);
+	_defaultClient.subscribeStateChange(success, error);
 }
 
 exports.unsubscribeStateChange = function(success, error) {
-	clientUnsubscribeStateChange(_defaultClientId, success, error);
+	_defaultClient.unsubscribeStateChange(success, error);
 }
 
 exports.peripheral_getServices = function(peripheral_id, success, error) {
-	clientPeripheral_getServices(_defaultClientId, peripheral_id, success, error);
+	_defaultClient.peripheral_getServices(peripheral_id, success, error);
 }
 
 
